885c80f1c25eed3f2d79b46849bce103ff4b880a
李铭昕 <715557344@qq.com>
2016-12-23 12:59:14 +0800

    * 增加hplus分页 列表 demo

--- app/controllers/admin/HplusController.php
+++ app/controllers/admin/HplusController.php
@@ -3,6 +3,7 @@
 namespace MyApp\Controllers\Admin;
 
 use MyApp\Controllers\ControllerBase;
+use MyApp\Models\Test\User;
 
 class HplusController extends ControllerBase
 {
@@ -15,6 +16,19 @@ class HplusController extends ControllerBase
     public function listAction()
     {
         return $this->view->render('admin/hplus', 'list');
+    }
+
+    public function pfnListAction()
+    {
+        $pageSize = $this->request->get('pageSize');
+        $pageIndex = $this->request->get('pageIndex');
+        $users = User::find([
+            'offset' => $pageIndex * $pageSize,
+            'limit' => $pageSize,
+            'columns' => "name,role_id,id",
+        ]);
+        $count = User::count();
+        return success(['data' => $users, 'count' => $count]);
     }
 
 }
