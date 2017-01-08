70e9fa2fcd03aa75e3e351f1fb909345a0ec7bdd
李铭昕 <715557344@qq.com>
2017-01-08 13:48:24 +0800

    * 增加命名空间注册

--- app/config/loader.php
+++ app/config/loader.php
@@ -9,9 +9,13 @@ $loader
     ->registerNamespaces(
         [
             'MyApp\Controllers' => $config->application->controllersDir,
+            'MyApp\Controllers\Admin' => $config->application->controllersDir . 'admin/',
+            'MyApp\Controllers\Test' => $config->application->controllersDir . 'test/',
             'MyApp\Models' => $config->application->modelsDir,
+            'MyApp\Models\Test' => $config->application->modelsDir . 'test/',
             'MyApp\Tasks' => $config->application->tasksDir,
             'MyApp\Tasks\System' => $config->application->tasksDir . 'system/',
+            'MyApp\Tasks\Test' => $config->application->tasksDir . 'test/',
             'MyApp\Traits' => $config->application->traitsDir,
             'MyApp\Traits\System' => $config->application->traitsDir . 'system/',
         ]
