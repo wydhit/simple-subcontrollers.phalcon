80cc7afd0d53f2c74e79134f74db42680785218a
李铭昕 <715557344@qq.com>
2017-02-03 16:26:49 +0800

    * 升级Rsa加密 解密算法 支持大文本

--- app/tasks/test/RsaTask.php
+++ app/tasks/test/RsaTask.php
@@ -17,27 +17,51 @@ class RsaTask extends Task
     public function mainAction()
     {
         $file = BASE_PATH . '/data/rsa/test/helloworld.php';
+        $this->encrypt($file);
+        $file = BASE_PATH . '/data/rsa/test/encrypted.php';
+        $this->decrypt($file);
+    }
+
+    private function decrypt($file)
+    {
+        $public_key = file_get_contents(BASE_PATH . '/data/rsa/rsa_public_key.pem');
+        $msg = file_get_contents($file);
+        $msg = json_decode($msg);
+        $result = '';
+        foreach ($msg as $item) {
+            // 公钥解密
+            openssl_public_decrypt(base64_decode($item), $decrypted, $public_key);
+            $result .= $decrypted;
+        }
+        $result = base64_decode($result);
+        // 存储解密文件
+        file_put_contents(BASE_PATH . '/data/rsa/test/decrypted.php', $result);
+    }
+
+    private function encrypt($file)
+    {
         $private_key = file_get_contents(BASE_PATH . '/data/rsa/rsa_private_key.pem');
 
         $msg = file_get_contents($file);
-        $private_key = openssl_pkey_get_private($private_key);
-        // 私钥加密
-        $ret = openssl_private_encrypt($msg, $encrypted, $private_key);
+        $msg = base64_encode($msg);
+        $len = strlen($msg);
+        $step = 110;
+        $result = [];
+        for ($i = 0; $i < $len;) {
+            $input = substr($msg, $i, $step);
+            $private_key = openssl_pkey_get_private($private_key);
+            // 私钥加密
+            $ret = openssl_private_encrypt($input, $encrypted, $private_key);
 
-        if (!$ret || empty($encrypted)) {
-            echo "fail to encrypt file md5";
+            if (!$ret || empty($encrypted)) {
+                echo "fail to encrypt file md5";
+                return;
+            }
+            // 存储加密文件
+            $encrypted = base64_encode($encrypted);
+            $result[] = $encrypted;
+            $i += $step;
         }
-        // 存储加密文件
-        $encrypted = base64_encode($encrypted);
-        file_put_contents(BASE_PATH . '/data/rsa/test/encrypted.php', $encrypted);
-
-        $public_key = file_get_contents(BASE_PATH . '/data/rsa/rsa_public_key.pem');
-        $file = BASE_PATH . '/data/rsa/test/encrypted.php';
-        $msg = file_get_contents($file);
-        // 公钥解密
-        openssl_public_decrypt(base64_decode($msg), $decrypted, $public_key);
-        // 存储解密文件
-        file_put_contents(BASE_PATH . '/data/rsa/test/decrypted.php', $decrypted);
-
+        file_put_contents(BASE_PATH . '/data/rsa/test/encrypted.php', json_encode($result));
     }
 }
--- data/rsa/test/decrypted.php
+++ data/rsa/test/decrypted.php
@@ -1,2 +1,6 @@
 <?php
 echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
