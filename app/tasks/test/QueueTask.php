ea5a60c49b507c38385c5421f977f522308a114c
李铭昕 <715557344@qq.com>
2017-02-03 22:23:06 +0800

    * 增加消息队列测试

--- app/tasks/test/QueueTask.php
+++ app/tasks/test/QueueTask.php
@@ -16,23 +16,36 @@ use limx\tools\LRedis;
 
 class QueueTask extends Task
 {
+    private $worker = [];
+
     public function mainAction()
     {
-        $config = [
-            'host' => '127.0.0.1',
-            'auth' => '',
-            'port' => '6379',
-        ];
-        $redis = LRedis::getInstance($config);
-        echo "queue begin" . PHP_EOL;
-        while (true) {
-            $res = $redis->brpop('phalcon:test:queue', 30);
-            if (empty($res)) {
-                echo "30s内没有消息进入！" . PHP_EOL;
-                continue;
-            }
-            print_r($res);
+        if (!extension_loaded('swoole')) {
+            echo "没有安装swoole扩展" . PHP_EOL;
+            return;
+        }
+        $worker_num = 2;//创建的进程数
+        for ($i = 0; $i < $worker_num; $i++) {
+            $process = new \swoole_process(function () {
+                $config = [
+                    'host' => '127.0.0.1',
+                    'auth' => '',
+                    'port' => '6379',
+                ];
+                $redis = LRedis::getInstance($config);
+                echo "queue begin" . PHP_EOL;
+                while (true) {
+                    $res = $redis->brpop('phalcon:test:queue', 30);
+                    if (empty($res)) {
+                        echo "30s内没有消息进入！" . PHP_EOL;
+                        continue;
+                    }
+                    print_r($res);
+                }
+            });
+            $pid = $process->start();
+            $this->worker[] = $process;
+            echo PHP_EOL . $pid;//
         }
     }
-
 }
