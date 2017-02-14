707049ba22b763b935f9808c89d81bdd5f0f9934
李铭昕 <715557344@qq.com>
2017-02-14 09:58:42 +0800

    * 增加array 脚本测试

--- app/tasks/test/ArrayTask.php
+++ app/tasks/test/ArrayTask.php
@@ -11,6 +11,8 @@ class ArrayTask extends Task
         $arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
         $res = array_chunk($arr, 3);
         print_r($res);
+        $res = array_chunk($arr, 20);
+        print_r($res);
     }
 
 }
