90cedeb4dc9b516dfc2d4e48f53cc656a9b4d75d
李铭昕 <715557344@qq.com>
2017-02-25 10:17:22 +0800

    * 增加WebSocket测试

--- app/tasks/swoole/TimerTask.php
+++ app/tasks/swoole/TimerTask.php
@@ -18,12 +18,12 @@ class TimerTask extends Task
     public function mainAction()
     {
         $id = swoole_timer_tick(1000, function () {
-            echo Color::colorize("Hello world", Color::FG_BLUE, Color::AT_BOLD) . PHP_EOL;
+            echo Color::colorize("Hello world", Color::FG_LIGHT_BLUE) . PHP_EOL;
         });
 
         swoole_timer_after(10000, function () use ($id) {
             swoole_timer_clear($id);
-            echo Color::colorize("Clear The Timer Id = " . $id, Color::FG_RED) . PHP_EOL;
+            echo Color::colorize("Clear The Timer Id = " . $id, Color::FG_LIGHT_RED) . PHP_EOL;
         });
     }
 }
