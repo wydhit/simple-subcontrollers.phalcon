28a0e6e13173d83b06635d7b59b8aca44dcc57a8
李铭昕 <715557344@qq.com>
2017-03-03 11:34:15 +0800

    * 增加Curl->get方法测试

---
+++ app/tasks/test/CurlTask.php
@@ -0,0 +1,51 @@
+<?php
+// +----------------------------------------------------------------------
+// | CurlTask Curl测试 [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016 http://www.lmx0536.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <http://www.lmx0536.cn>
+// +----------------------------------------------------------------------
+// | Date: 2017/3/3 Time: 上午11:18
+// +----------------------------------------------------------------------
+namespace MyApp\Tasks\Test;
+
+use Phalcon\Cli\Task;
+use limx\phalcon\Cli\Color;
+
+class CurlTask extends Task
+{
+    public function mainAction()
+    {
+        echo Color::head('Help:') . PHP_EOL;
+        echo Color::colorize('  Curl测试') . PHP_EOL . PHP_EOL;
+
+        echo Color::head('Usage:') . PHP_EOL;
+        echo Color::colorize('  php run Test\\\\Curl [action]', Color::FG_GREEN) . PHP_EOL . PHP_EOL;
+
+        echo Color::head('Actions:') . PHP_EOL;
+        echo Color::colorize('  get     get方法样例 - 获取百度首页页面', Color::FG_GREEN) . PHP_EOL;
+    }
+
+    public function getAction()
+    {
+        $ch = curl_init();
+        // 设置抓取的url
+        curl_setopt($ch, CURLOPT_URL, 'http://www.baidu.com');
+        // 启用时会将头文件的信息作为数据流输出。
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        // 启用时将获取的信息以文件流的形式返回，而不是直接输出。
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // 启用时会将服务器服务器返回的"Location: "放在header中递归的返回给服务器，使用CURLOPT_MAXREDIRS可以限定递归返回的数量。
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        //执行命令
+        $result = curl_exec($ch);
+        if ($result === false) {
+            echo 'Curl error: ' . curl_error($ch);
+            return false;
+        }
+        //关闭URL请求
+        curl_close($ch);
+        echo $result;
+    }
+}
