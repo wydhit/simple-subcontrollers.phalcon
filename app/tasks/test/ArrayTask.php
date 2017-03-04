0969bea9de145edeada30b3fe1cc0adbca963bc5
李铭昕 <715557344@qq.com>
2017-03-04 23:07:25 +0800

    * 增加Array 函数测试

--- app/tasks/test/ArrayTask.php
+++ app/tasks/test/ArrayTask.php
@@ -18,7 +18,28 @@ class ArrayTask extends Task
         echo Color::head('Actions:') . PHP_EOL;
         echo Color::colorize('  chunk                   数组重新分组', Color::FG_GREEN) . PHP_EOL;
         echo Color::colorize('  case    [upper|lower]   修改键名为全大写或小写', Color::FG_GREEN) . PHP_EOL;
+        echo Color::colorize('  combine                 拼接key数组和val数组', Color::FG_GREEN) . PHP_EOL;
+        echo Color::colorize('  count                   统计数组中所有的值出现的次数', Color::FG_GREEN) . PHP_EOL;
 
+    }
+
+    public function countAction()
+    {
+        $data = [1, 2, 'hello', 1, 'hello', 'hello', '1'];
+        echo Color::head("原数组：") . PHP_EOL;
+        echo Color::colorize(json_encode($data), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::colorize(json_encode(array_count_values($data)), Color::FG_LIGHT_GREEN) . PHP_EOL;
+    }
+
+    public function combineAction()
+    {
+        $key = ['key1', 'key2', 'key3'];
+        $val = ['val1', 'val2', 'val3'];
+        echo Color::head("原数组：") . PHP_EOL;
+        echo Color::colorize(json_encode($key), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::colorize(json_encode($val), Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::head("结果：") . PHP_EOL;
+        echo Color::colorize(json_encode(array_combine($key, $val)), Color::FG_LIGHT_GREEN) . PHP_EOL;
     }
 
     public function caseAction($params)
