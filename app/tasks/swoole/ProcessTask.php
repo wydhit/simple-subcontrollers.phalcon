798d81231d7ce2b06905e70c0ecfeb86c86ef170
李铭昕 <715557344@qq.com>
2017-04-23 21:30:14 +0800

    * 修改swoole多进程测试

--- app/tasks/swoole/ProcessTask.php
+++ app/tasks/swoole/ProcessTask.php
@@ -8,30 +8,43 @@
 // +----------------------------------------------------------------------
 // | Date: 2017/2/23 Time: 下午2:39
 // +----------------------------------------------------------------------
-declare(ticks = 1);
+declare(ticks=1);
+
 namespace App\Tasks\Swoole;
 
 use Phalcon\Cli\Task;
 use limx\phalcon\Cli\Color;
+use swoole_process;
 
 class ProcessTask extends Task
 {
+    protected $process = 0;
+
     public function mainAction()
     {
         pcntl_signal(SIGCHLD, [$this, "signalHandler"]);
         for ($i = 0; $i < 10; $i++) {
-            $process = new \swoole_process([$this, 'task']);
+            $process = new swoole_process([$this, 'task']);
             $date = uniqid();
             $process->write($date);
-            $process->start();
+            if ($process->start()) {
+                $this->process++;
+            }
         }
-        sleep(100);
+        while (true) {
+            echo $this->process, PHP_EOL;
+            if ($this->process <= 0) {
+                break;
+            }
+            sleep(3);
+        }
     }
 
-    public function task(\swoole_process $worker)
+    public function task(swoole_process $worker)
     {
         // $data = $worker->read();
-        // echo Color::colorize($data, Color::FG_RED) . PHP_EOL;
+        // echo Color::colorize($data, Color::FG_LIGHT_CYAN) . PHP_EOL;
+        // echo Color::colorize("FINISH", COlor::FG_LIGHT_BLUE) . PHP_EOL;
         // $worker->exit(0);
 
         swoole_event_add($worker->pipe, function ($pipe) use ($worker) {
@@ -46,7 +59,7 @@ class ProcessTask extends Task
     }
 
     /**
-     * @desc 信号处理方法 回收已经dead的子进程
+     * @desc   信号处理方法 回收已经dead的子进程
      * @author limx
      * @param $signo
      */
@@ -54,7 +67,8 @@ class ProcessTask extends Task
     {
         switch ($signo) {
             case SIGCHLD:
-                while ($ret = \swoole_process::wait(false)) {
+                while (swoole_process::wait(false)) {
+                    logger("kill deadprocess successful! ID=" . ($this->process--));
                     echo Color::colorize("kill deadprocess successful!", Color::FG_LIGHT_RED) . PHP_EOL;
                 }
         }
