70c8c22b377a4e2cc648a964d68319775d56ce8d
李铭昕 <715557344@qq.com>
2017-04-26 15:17:52 +0800

    * date(“W”)测试

--- app/tasks/test/DateTask.php
+++ app/tasks/test/DateTask.php
@@ -23,6 +23,15 @@ class DateTask extends Task
 
         echo Color::head('Actions:'), PHP_EOL;
         echo Color::colorize('  format              format测试', Color::FG_GREEN), PHP_EOL;
+        echo Color::colorize('  week                第几年的第几周', Color::FG_GREEN), PHP_EOL;
+    }
+
+    public function weekAction()
+    {
+        $week = date("W");
+        $msg = sprintf("今天是一年的第%d周", $week);
+        echo Color::colorize($msg, Color::FG_LIGHT_CYAN), PHP_EOL;
+
     }
 
     public function formatAction()
