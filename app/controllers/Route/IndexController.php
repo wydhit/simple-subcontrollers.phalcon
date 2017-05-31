69cb4c8e96083651db7c378745b9cf8967fcb6f7
李铭昕 <715557344@qq.com>
2017-05-31 17:31:38 +0800

    * 增加路由命名测试

--- app/controllers/Route/IndexController.php
+++ app/controllers/Route/IndexController.php
@@ -19,9 +19,13 @@ class IndexController extends Controller
 
     public function nameAction()
     {
+        // dump($this->router->getRoutes());exit;
+        // dump($this->router->getRouteByName('route.index.target'));exit;
         $url = $this->url->get(['for' => 'route.index.target']);
         // return $this->response->redirect(['for' => 'route.index.target']);
         echo "路由命名测试: " . $url;
+
+        return $this->view->render('test/view', 'name');
     }
 
     public function targetAction()
